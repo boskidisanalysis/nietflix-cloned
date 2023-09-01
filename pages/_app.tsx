8751426c20f06c2f35090f90b07293320ac63939
boskidisanalysis <boskidisdevelopment@gmail.com>
2023-09-02 00:39:27 +0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import { SessionProvider } from "next-auth/react"
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
-}) {
+}: AppProps) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
